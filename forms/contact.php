0a995c4b1c47aaa3b41a1bd1a82f892aedf62dc5
SaadMalik07 <70927435+SaadMalik07@users.noreply.github.com>
2020-10-05 18:33:45 -0500

    Update contact.php
    adding email capability

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,16 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.gmail.com',
+    'username' => 'saadmalik@princeton.edu',
+    'password' => 'STigerMalik0077',
+    'port' => '587',
+    'mail'=>SMTPSecure = 'tls',
+    'mail'=>SMTPAuth = true
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
